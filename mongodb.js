2a5c40cdfebffac1e180042c0f10cbcec1ad2185
Patricio R <patricio.raschetti@gmail.com>
2019-12-19 18:51:28 -0300

    Inserting docs

--- mongodb.js
+++ mongodb.js
@@ -7,13 +7,37 @@ const dbName = 'task-manager';
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
-        return console.log('Could not connect to the DB!');
+        return console.log('Could not connect to the DB!', error);
     };
 
     const db = client.db(dbName);
     
-    db.collection('users').insertOne({
-        name: 'Patou',
-        age: 31
-    });
+    // db.collection('users').insertOne({
+    //     name: 'Patanego',
+    //     age: 13*7
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert document (user).', error);
+    //     };
+    //     console.log(result.ops);
+    // });
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Papá',
+    //         age: 63
+    //     },
+    //     {
+    //         name: 'Mama',
+    //         age: 60
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents (users).', error);
+    //     };
+
+    //     console.log(result.ops);
+    // });
+
+
 });
