5c06c38ac7b39b7b1551ff031a24ec4c155c6ce2
Patricio R <patricio.raschetti@gmail.com>
2019-12-22 00:17:59 -0300

    Create resource basics 2

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,15 +2,3 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String, 
-        trim: true,
-        unique: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Configs.
 app.use(express.json());
 
+// Creates.
 app.post('/users', (req, res) => {
     const user = new User(req.body);
     user.save().then(user => {
         res.status(201).send(user);
+    }).catch(e => {
+        res.status(400).send(e);
+    });
+});
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+    task.save().then(task => {
+        res.status(201).send(task);
     }).catch(e => {
         res.status(400).send(e);
     });
