6874e231e15f124bbd70c003fde9f9ff14f0764e
Patricio R <patricio.raschetti@gmail.com>
2019-12-23 17:47:57 -0300

    Update endpoint I

--- src/index.js
+++ src/index.js
@@ -60,9 +60,8 @@ app.get('/tasks', async (req, res) => {
 // Endpoints: READ ONE.
 // GET Users ID
 app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
     try {
-        const user = await User.findById(_id);
+        const user = await User.findById(req.params.id);
         if (!user) {
             return res.status(404).send();
         };
@@ -74,9 +73,8 @@ app.get('/users/:id', async (req, res) => {
 
 // GET Tasks ID
 app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id;
     try {
-        const task = await Task.findById(_id);
+        const task = await Task.findById(req.params.id);
         if (!task) {
             return res.status(404).send();
         };
@@ -86,6 +84,27 @@ app.get('/tasks/:id', async (req, res) => {
     };
 });
 
+
+// Endpoints: UPGRADE ONE
+// Users
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'age', 'email', 'password'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid update within.'});
+    };
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+        if (!user) {
+            return res.status(404).send();
+        };
+        res.send(user);
+    } catch (e) {
+        res.status(400).send(e);
+    };
+});
 
 
 
