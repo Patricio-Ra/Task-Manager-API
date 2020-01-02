c43bbf83fadf57d0970672af1d21bb16f908a2a9
Patricio R <patricio.raschetti@gmail.com>
2020-01-02 11:00:09 -0300

    Add basic sorting. Refactor GET /tasks route with ES6+

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,17 +18,29 @@ router.post('/tasks', auth, async (req, res) => {
 
 router.get('/tasks', auth, async (req, res) => {
     const match = {};
-    const completed = req.query.completed;
+    const sort = {};
+    let { sortBy, completed, limit, skip } = req.query;
+    limit = parseInt(limit);
+    skip = parseInt(skip);
+
     if (completed) {
-        match.completed = completed;
+        if (completed.toLowerCase() === 'true' || completed.toLowerCase() === 'false') {
+            match.completed = completed;
+        };
     };
+    if (sortBy) {
+        const [field, direction] = sortBy.split(/_|:/);
+        sort[field] = direction.toLowerCase() === 'desc' ? -1 : 1;
+    };
+
     try {
         await req.user.populate({
             path: 'tasks',
             match,
             options: {
-                limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                limit,
+                skip,
+                sort
             }
         }).execPopulate();
         res.send(req.user.tasks);
