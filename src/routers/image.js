7a5a9c371de2ea0e4ebdebbae22bec331b2e93e4
Patricio R <patricio.raschetti@gmail.com>
2020-01-07 02:13:56 -0300

    Img resizer finish

--- src/routers/image.js
+++ src/routers/image.js
@@ -5,15 +5,19 @@ const router = new express.Router();
 
 router.post('/image/resize', uploadImage.single('image'), async (req, res) => {
     try {
-        await sharp(req.file.path).resize({ width: +req.body.width, height: +req.body.height}).toFile(`images/resized/image.png`, (e, data) => {
+        await sharp(req.file.path).resize({ 
+            width: +req.body.width, 
+            height: +req.body.height,
+            fit: req.body.fit
+        }).toFile(`images/resized/${req.body.name}-${req.body.fit}.png`, (e, data) => {
             if (e) {
-                console.log(e);
+                res.send(e);
             } else {
                 res.send();
             };
         });
     } catch (e) {
-        res.status(400).send();
+        res.status(400).send(e);
     };
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message });
