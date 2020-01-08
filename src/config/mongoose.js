7f97a70b0059c074f1e992cd09b30526b15551b1
Patricio R <patricio.raschetti@gmail.com>
2020-01-08 16:30:02 -0300

    Little refactoring

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -6,5 +6,10 @@ mongoose.connect(process.env.MONGODB_URL,
         useUnifiedTopology: true, 
         useCreateIndex: true,
         useFindAndModify: false
+    })
+    .then(connection => {
+        console.log('DB connection stablish..');
+    })
+    .catch(error => {
+        console.log('Could not connect to DB: ' + error);
     });
-
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-require('./db/mongoose');
+require('./config/mongoose');
 const express = require('express');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 const imageRouter = require('./routers/image');
 
 const app = express();
-const port = process.env.PORT;
-
+const PORT = process.env.PORT;
 
 // Body-Parser.
 app.use(express.json());
@@ -15,8 +14,7 @@ app.use(userRouter);
 app.use(taskRouter);
 app.use(imageRouter);
 
-
 // Listen
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
